9d3ba228cc6db4a8b35f7bc518f54f515f8095c7
Jinyoung Kim <jinyk226@gmail.com>
2022-02-08 22:51:45 -0800

    reverting to standard serviceworker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,37 +1,49 @@
 const CACHE_NAME = "version-1";
 const urlsToCache = [ 'index.html', 'offline.html' ];
-const self = this
+
+const self = this;
 
 // Install SW
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-    e.waitUntil((async () => {
-      const cache = await caches.open(CACHE_NAME);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(urlsToCache);
-    })());
-  });
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+
+                return cache.addAll(urlsToCache);
+            })
+    )
+});
 
 // Listen for requests
-self.addEventListener('fetch', (e) => {
-    e.respondWith((async () => {
-      const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (r) { return r; }
-      const response = await fetch(e.request);
-      const cache = await caches.open(CACHE_NAME);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-  });
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request) 
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
+    if (event.request.mode === 'navigate') {
+        return event.respondWith(
+          fetch(event.request).catch(() => caches.match('offline.html'))
+        );
+      }
+});
 
 // Activate the SW
-self.addEventListener('activate', (e) => {
-    e.waitUntil(caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key === CACHE_NAME) return
-        return caches.delete(key);
-      }))
-    }));
-  });
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+            
+    )
+});
